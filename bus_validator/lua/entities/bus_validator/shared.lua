578d74e895567872ab8e8daabcd28cbf9815418d
FURUSHKA <163753122+FYURi4@users.noreply.github.com>
2025-05-04 05:23:29 -0700

    Create shared.lua

---
+++ bus_validator/lua/entities/bus_validator/shared.lua
@@ -0,0 +1,21 @@
+ENT.Base = "base_gmodentity"
+
+ENT.Type = "anim"
+
+ENT.Category = "3DEMC_ENT"
+
+ENT.Spawnable = true
+
+ENT.AdminSpawnable = true
+
+ENT.ClassName = "Bus_Validator"
+
+ENT.PrintName = "Bus_Validator"
+
+ENT.Author = "FURA"
+
+ENT.Contact = "Discord: fyurl4, Furushka: FurichF"
+
+ENT.Purpose = "Создание блинчиков."
+
+ENT.Instructions = "Подойти к модели, нажать 'E'. "
